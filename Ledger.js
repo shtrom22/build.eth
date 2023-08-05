f0c30cb4baa078e8072d9777d523411e97143361
shtrom22 <112576423+shtrom22@users.noreply.github.com>
2023-08-05 15:30:20 +0300

    Update Ledger.js

--- Ledger.js
+++ Ledger.js
@@ -1,2 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { withStyles } from '@material-ui/core/styles';
+import { Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
