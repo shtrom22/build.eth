0d906511502c8298ed1fbb784b6576b83d3f581a
shtrom22 <112576423+shtrom22@users.noreply.github.com>
2023-08-06 12:28:39 +0300

    Update Ledger.js

--- Ledger.js
+++ Ledger.js
@@ -2,3 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { withStyles } from '@material-ui/core/styles';
 import { Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import Blockies from 'react-blockies';
+
+const StyledTableRow = withStyles(theme => ({
